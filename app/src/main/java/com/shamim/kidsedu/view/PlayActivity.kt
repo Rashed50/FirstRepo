59e71c57009ed420208ec84cc4a1fdc18867feef
shamimcse1 <mdshamimh529@gmail.com>
2023-10-17 22:37:36 +0600

    autoplay

--- app/src/main/java/com/shamim/kidsedu/view/PlayActivity.kt
+++ app/src/main/java/com/shamim/kidsedu/view/PlayActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
 import com.google.gson.Gson
 import com.shamim.kidsedu.R
@@ -18,6 +19,9 @@ import com.shamim.kidsedu.databinding.ActivityPlayBinding
 import com.shamim.kidsedu.model.MainDataModelItem
 import com.shamim.kidsedu.view.adapter.PlayAdapter
 import java.io.IOException
+import java.text.BreakIterator
+import java.util.Timer
+import java.util.TimerTask
 
 
 class PlayActivity : AppCompatActivity() {
@@ -28,8 +32,9 @@ class PlayActivity : AppCompatActivity() {
 
     private var currentPlayingPosition = 0
     private var isSound = false
+    private var isAutoPlay = true
     private val data = ArrayList<MainDataModelItem>()
-
+    private var timer : Timer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -38,6 +43,8 @@ class PlayActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_play)
+
+        timer = Timer()
 
         val category: Int = intent.getIntExtra("key", 0)
 
@@ -49,10 +56,10 @@ class PlayActivity : AppCompatActivity() {
                 data.add(i)
             }
         }
-//        val audio = data[currentPlayingPosition].image_sound + ".mp3"
-//        startSound(audio)
+
         Log.d("data", data.toString())
         Log.d("data", category.toString())
+
         adapter = PlayAdapter(this, data)
 
         _binding.playRecyclerview.adapter = adapter
@@ -93,30 +100,30 @@ class PlayActivity : AppCompatActivity() {
 
         }
         _binding.nextBtn.setOnClickListener {
-            if (layoutManager.findLastCompletelyVisibleItemPosition() < (adapter.itemCount - 1)) {
-                layoutManager.scrollToPosition(layoutManager.findLastCompletelyVisibleItemPosition() + 1)
-                currentPlayingPosition++
-                if (data[currentPlayingPosition].image_sound != null) {
-                    val audio = data[currentPlayingPosition].image_sound + ".mp3"
-                    startSound(audio)
-                }
 
-                try {
-
-                } catch (e: Exception) {
-                    e.printStackTrace()
+            try {
+                if (layoutManager.findLastCompletelyVisibleItemPosition() < (adapter.itemCount - 1)) {
+                    layoutManager.scrollToPosition(layoutManager.findLastCompletelyVisibleItemPosition() + 1)
+                    currentPlayingPosition++
+                    if (!currentPlayingPosition.toString().contains("-")){
+                        if (data[currentPlayingPosition].image_sound != null) {
+                            val audio = data[currentPlayingPosition].image_sound + ".mp3"
+                            startSound(audio)
+                        }
+                    }
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-
         }
 
         _binding.soundBtn.setOnClickListener {
             if (isSound) {
                 isSound = false
-                music(isSound)
+                music(false)
             } else {
                 isSound = true
-                music(isSound)
+                music(true)
             }
 
         }
@@ -125,22 +132,49 @@ class PlayActivity : AppCompatActivity() {
             finish()
         }
 
+        var m = 1
         _binding.autoPlay.setOnClickListener {
-            startSound("A.mp3")
+            if (m ==1){
+
+                val layoutManager1: LinearLayoutManager = object : LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false){
+                    override fun canScrollVertically(): Boolean {
+                        return false
+                    }
+
+                    override fun canScrollHorizontally(): Boolean {
+                        return true
+                    }
+                }
+                _binding.playRecyclerview.layoutManager = layoutManager1
+
+                _binding.autoPlay.setImageResource(R.drawable.slideshowon)
+                Toast.makeText(this, "1111111", Toast.LENGTH_SHORT).show()
+
+                timer?.schedule(object : TimerTask() {
+                    override fun run() {
+                        if ((_binding.playRecyclerview.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition() < adapter.itemCount-1) {
+
+                            (_binding.playRecyclerview.layoutManager as LinearLayoutManager).smoothScrollToPosition(
+                                _binding.playRecyclerview, RecyclerView.State(),
+                                (_binding.playRecyclerview.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition() + 1
+                            )
+
+                            if (data[currentPlayingPosition].image_sound != null) {
+                                val audio = data[currentPlayingPosition].image_sound + ".mp3"
+                                startSound(audio)
+                                currentPlayingPosition++
+                            }
+                        }
+                        Log.d("ddd", "Auto Play $currentPlayingPosition")
+                    }
+                }, 0, 3000)
+            }
+            m = 0
+
         }
+
     }
 
-    fun autoPlay() {
-
-        mediaPlayer?.setOnPreparedListener {
-
-            val duration = mediaPlayer?.duration
-            val durationInSeconds = (duration?.div(1000))
-            Log.d("audio", durationInSeconds.toString())
-
-        }
-
-    }
 
     private fun startSound(filename: String) {
         var afd: AssetFileDescriptor? = null
@@ -204,5 +238,12 @@ class PlayActivity : AppCompatActivity() {
 
         val listCategoryType = object : TypeToken<List<MainDataModelItem>>() {}.type
         return Gson().fromJson(jsonString, listCategoryType)
+    }
+
+    override fun onStop() {
+        stopMedia()
+        timer?.cancel()
+        currentPlayingPosition = 0
+        super.onStop()
     }
 }
